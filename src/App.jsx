43c647aeaf30ee5d4196ab5fb423c72895676112
C4r1xs <carlos.dimitrakis@tecnologicoliceocristiano.edu.ec>
2024-06-06 23:52:42 -0500

    Realizamos lo siguiente -Al momento de dar delete muestra el mensaje de confirmacion -Y al momento de dar "proceder" desaparece el cuadro de advertencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
+import { useState } from 'react'
 import { Instructions } from './components/Instructions'
 import './App.css'
 
 function App() {
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleShowWarning = () => {
+    setShowWarning(true);
+  };
+
+  const handleHideWarning = () => {
+    setShowWarning(false);
+  };
   
   return (
     <>
       <section id='instructions'>
         <Instructions/>
       </section>
-      
       <div>
+      {showWarning && (
         <div data-testid="alert" id="alert">
           <h2>Are you sure?</h2>
           <p>These changes can't be reverted!</p>
-          <button>Proceed</button>
-        </div>
-        <button>Delete</button>
+          <button onClick={handleHideWarning}>Proceed</button>
+        </div>)}
+        <button onClick={handleShowWarning}>Delete</button>
       </div>    
     </>
+  /*     Usamos show aqui por que es una variable de estado boolean y puede ser true o false
+  Si showWarning es true, se renderiza el contenido dentro de los paréntesis.
+  Si showWarning es false, no se renderiza nada de lo que está dentro de los paréntesis.
+  El operador lógico && / AND en JavaScript devuelve el primer valor falso que encuentra 
+  o el último valor si todos son verdaderos. -Carlos Dimitrakis*/
   )
 }
 
